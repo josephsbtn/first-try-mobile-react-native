ea78f7b5d457b5e7fd1fc421f6237b62a520fb0c
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2025-05-17 18:51:25 +0700

    SETUP REACT NATIVEWIND

---
+++ movieapp/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-400">
+      <Text className="text-3xl font-bold underline text-white">
+        Hello world!
+      </Text>
+    </View>
+  );
+}
